14754b3d9bb53d9121743e94fe420de04d53e586
mateusangeljr <mateusangeljr@gmail.com>
2026-09-06 11:25:21 -0300

    ajuste

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -7,7 +7,8 @@ var map = new ol.Map({
     }),
     controls: ol.control.defaults({
         zoom: false,
-        rotate: false
+        rotate: false,
+        attribution: false
     })
 });
 
@@ -461,26 +462,6 @@ var bottomRightContainerDiv = document.getElementById('bottom-right-container')
 
 
 
-
-
-//attribution
-var bottomAttribution = new ol.control.Attribution({
-  collapsible: false,
-  collapsed: false,
-  className: 'bottom-attribution'
-});
-map.addControl(bottomAttribution);
-
-var attributionList = document.createElement('li');
-// attributionList.innerHTML = `
-// 	<a href="https://github.com/qgis2web/qgis2web">qgis2web</a> &middot;
-// 	<a href="https://openlayers.org/">OpenLayers</a> &middot;
-// 	<a href="https://qgis.org/">QGIS</a>	
-// `;
-var bottomAttributionUl = bottomAttribution.element.querySelector('ul');
-if (bottomAttributionUl) {
-  bottomAttribution.element.insertBefore(attributionList, bottomAttributionUl);
-}
 
 
 // Disable "popup on hover" or "highlight on hover" if ol-control mouseover
